44d985485a242824a37ad641775b4833d694908d
Michael Ruhaak <67177569+Sinrock@users.noreply.github.com>
2020-07-21 19:55:46 -0600

    adding repo
    Alternative method for adding repo to github test 3.

--- lib/strainer.rb
+++ lib/strainer.rb
@@ -2,5 +2,9 @@ require "strainer/version"
 
 module Strainer
   class Error < StandardError; end
-  # Your code goes here...
+  class Test
+    def say_hello
+      puts "Hello World!"
+    end
+  end
 end
