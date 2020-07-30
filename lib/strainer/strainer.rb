212d6bbeff15a9f4f0f69fbabd3a224c76d57a96
Sinrock <mruhaak@gmail.com>
2020-07-30 03:38:35 -0600

    added a thank you message

--- lib/strainer/strainer.rb
+++ lib/strainer/strainer.rb
@@ -89,6 +89,8 @@ class All
     end
 end
 
+thanks = "Thanks for using Strainer!"
+
 if user_choice == 'Name'
     choice = gets.chomp
     name = Name.new("http://strainapi.evanbusse.com/rvxnT8j/strains/search/name/#{choice}")
@@ -99,28 +101,32 @@ elsif user_choice == 'Effect' #I want to get this list pulled in from the API, n
     effect_choice = gets.chomp
     effects = Effects.new("http://strainapi.evanbusse.com/rvxnT8j/strains/search/effect/#{effect_choice}")
     puts effects.parse_json
+    puts thanks
 
 elsif user_choice == 'Flavor' #I want to get this list pulled in from the API, not hardcoded in
   puts "Choices are: Earthy, Chemical, Pine, Spicy/Herbal, Pungent, Pepper, Flowery, Citrus, Orange, Sweet, Skunk, Grape, Minty, Woody, Cheese, Diesel, Tropical, Grapefruit, Nutty, Lemon, Berry, Blueberry, Ammonia, Apple, Rose, Butter, Honey, Tea, Lime, Lavender, Strawberry, Mint, Chetnut, Tree Fruit, Pear, Apricot, Peach, Blue Cheese, Menthol, Coffee, Tar, Mango, Pineapple, Sage, Vanilla, Plum Tobacco, Violet."
     flavor_choice = gets.chomp
     flavors = Flavors.new("http://strainapi.evanbusse.com/rvxnT8j/strains/search/flavor/#{flavor_choice}")
     puts flavors.parse_json
+    puts thanks
 
 elsif user_choice == 'Race' #I'm okay with this list being hardcoded however. :)
     puts "Choices are Indica, Sativa & Hybrid"
     race_choice = gets.chomp
     race = Race.new("http://strainapi.evanbusse.com/rvxnT8j/strains/search/race/#{race_choice}")
     puts race.parse_json
+    puts thanks
 
-elsif user_choice == 'ALL'
+elsif user_choice == 'ALL' #The developer of The Strain API DOES indeed recommend NOT running this  too much as its very resource intensive. 
     prompt = TTY::Prompt.new
     all_selection = prompt.select("This is a HUGE list, are you sure?", %w(Yes No))
 if all_selection == 'Yes'
     put_it_all = All.new("http://strainapi.evanbusse.com/rvxnT8j/strains/search/all")
     puts put_it_all.parse_json
+    puts thanks
 
 else puts "Good call! ;)"
-  
+
 end
 end
 end
